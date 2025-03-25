cfa79125fa3bea86ba6e73262cd915fb955dff01
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-03-25 17:37:01 +0000

    added restaurants card grid

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,23 @@ import "./App.css";
 const PORT = import.meta.env.VITE_PORT;
 
 function App() {
-  
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:${PORT}/restaurants`);
+
+        const data = await response.json();
+
+        setRestaurants(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <main className="container">
       <header style={{ border: "2px solid black" }}>Header</header>
@@ -13,7 +29,13 @@ function App() {
         className="overflow-container"
         style={{ border: "2px solid lightblue" }}
       >
-        
+        <ul className="restaurants-grid">
+          {restaurants.map((restaurant, index) => (
+            <li key={index} className="restaurant-card">
+              <div>Restaurant Info</div>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
